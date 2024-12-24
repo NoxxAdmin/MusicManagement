16683c2a31798b902231631b861d24b39a39c65a
Highpower143 <nandyadu1c@gmail.com>
2024-12-24 16:59:35 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -67,7 +67,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Nand7280/Rdp",
+    "https://github.com/Nand72820/Rdp",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
